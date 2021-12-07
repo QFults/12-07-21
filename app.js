077b6ea354ae01ea5562ed4d799065dec2db6fd2
Quinton Fults <quinton.fults.work@gmail.com>
2021-12-07 11:56:49 -0800

    code

--- app.js
+++ app.js
@@ -13,6 +13,14 @@
 //   }
 // }
 
+// function Mod (name, username, email, phone) {
+//   User.call(this, name, username, email)
+//   this.phone = phone
+//   this.canBan = true
+//   this.canKick = true
+//   this.canMove = true
+// }
+
 // const user1 = new User('John Doe', 'johndoe', 'johndoe@gmail.com')
 // const user2 = new User('Jane Doe', 'janedoe', 'janedoe@gmail.com')
 
@@ -24,6 +32,10 @@
 
 // user1.printInfo()
 // user2.printInfo()
+
+// const mod1 = new Mod('Jack Doe', 'jackdoe', 'jackdoe@gmail.com', 1234567890)
+
+// console.log(mod1)
 
 // class User {
 //   constructor(name, username, email) {
@@ -43,6 +55,16 @@
 //   }
 // }
 
+// class Mod extends User {
+//   constructor(name, username, email, phone) {
+//     super(name, username, email)
+//     this.phone = phone
+//     this.canBan = true
+//     this.canKick = true
+//     this.canMove = true
+//   }
+// }
+
 // const user1 = new User('John Doe', 'johndoe', 'johndoe@gmail.com')
 // const user2 = new User('Jane Doe', 'janedoe', 'janedoe@gmail.com')
 
@@ -55,27 +77,32 @@
 // user1.printInfo()
 // user2.printInfo()
 
-const user = (name, username, email) => ({
-    name, username, email,
-    level: 1,
-    printInfo() {
-      console.log(`
-      Name: ${this.name}
-      Username: ${this.username}
-      Email: ${this.email}
-      Level: ${this.level}
-    `)
-    }
-  })
+// const mod1 = new Mod('Jack Doe', 'jackdoe', 'jackdoe@gmail.com', 1234567890)
 
-const user1 = user('John Doe', 'johndoe', 'johndoe@gmail.com')
-const user2 = user('Jane Doe', 'janedoe', 'janedoe@gmail.com')
+// console.log(mod1)
 
-user2.email = 'something else'
-user2.level = 2
 
-console.log(user1)
-console.log(user2)
+// const user = (name, username, email) => ({
+//     name, username, email,
+//     level: 1,
+//     printInfo() {
+//       console.log(`
+//       Name: ${this.name}
+//       Username: ${this.username}
+//       Email: ${this.email}
+//       Level: ${this.level}
+//     `)
+//     }
+//   })
 
-user1.printInfo()
-user2.printInfo()
+// const user1 = user('John Doe', 'johndoe', 'johndoe@gmail.com')
+// const user2 = user('Jane Doe', 'janedoe', 'janedoe@gmail.com')
+
+// user2.email = 'something else'
+// user2.level = 2
+
+// console.log(user1)
+// console.log(user2)
+
+// user1.printInfo()
+// user2.printInfo()
