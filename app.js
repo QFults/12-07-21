97cd73b4d5090f9f34bb041599936d30b5933311
Quinton Fults <quinton.fults.work@gmail.com>
2021-12-07 10:46:15 -0800

    code

---
+++ app.js
@@ -0,0 +1,56 @@
+// function User (name, username, email) {
+//   this.name = name
+//   this.username = username
+//   this.email = email
+//   this.level = 1
+//   this.printInfo = function () {
+//     console.log(`
+//       Name: ${this.name}
+//       Username: ${this.username}
+//       Email: ${this.email}
+//       Level: ${this.level}
+//     `)
+//   }
+// }
+
+// const user1 = new User('John Doe', 'johndoe', 'johndoe@gmail.com')
+// const user2 = new User('Jane Doe', 'janedoe', 'janedoe@gmail.com')
+
+// user2.email = 'something else'
+// user2.level = 2
+
+// console.log(user1)
+// console.log(user2)
+
+// user1.printInfo()
+// user2.printInfo()
+
+// class User {
+//   constructor(name, username, email) {
+//     this.name = name
+//     this.username = username
+//     this.email = email
+//     this.level = 1
+//   }
+
+//   printInfo() {
+//     console.log(`
+//       Name: ${this.name}
+//       Username: ${this.username}
+//       Email: ${this.email}
+//       Level: ${this.level}
+//     `)
+//   }
+// }
+
+// const user1 = new User('John Doe', 'johndoe', 'johndoe@gmail.com')
+// const user2 = new User('Jane Doe', 'janedoe', 'janedoe@gmail.com')
+
+// user2.email = 'something else'
+// user2.level = 2
+
+// console.log(user1)
+// console.log(user2)
+
+// user1.printInfo()
+// user2.printInfo()
